d35814636f32ac52367f8fafe21c6950ca8a278a
陈华狄 <861883474@qq.com>
2018-07-27 16:19:24 +0800

    v

--- dist/ddv-multi-window.esm.js
+++ dist/ddv-multi-window.esm.js
@@ -1,5 +1,5 @@
 /*!
-  * ddv-multi-window v0.1.2
+  * ddv-multi-window v0.1.0
   * (c) 2018 yuchonghua@163.com
   * @license MIT
   */
@@ -3569,7 +3569,7 @@ DdvMultiWindowGlobal.prototype.hasOwnProperty('namespace') || Object.definePrope
 var g = Object.assign((new DdvMultiWindowGlobal()), {
   isDaemon: true,
   Ready: Ready,
-  version: '0.1.2'
+  version: '0.1.0'
 });
 globalInit(g);
 
