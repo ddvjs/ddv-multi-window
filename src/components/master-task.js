4d2cbc144f0c14316a701ac411ede2bd0cb08e2e
hua <yuchonghua@163.com>
2018-08-04 08:57:45 +0800

    Add getOptions

--- src/components/master-task.js
+++ src/components/master-task.js
@@ -1,20 +1,2 @@
-import master from './master'
-export default {
-  name: 'ddv-multi-window-task',
-  functional: true,
-  props: {
-    daemonId: {
-      type: [Number, String],
-      default: 'daemon'
-    },
-    autoColse: {
-      type: Boolean,
-      default: true
-    }
-  },
-  render (h, { data, children, props }) {
-    props.view = 'task'
-    data.props = props
-    return h(master, data, children)
-  }
-}
+import { getOptions } from './master'
+export default getOptions('ddv-multi-window-task', 'task')
--- src/components/master-view.js
+++ src/components/master-view.js
@@ -1,20 +1,2 @@
-import master from './master'
-export default {
-  name: 'ddv-multi-window-view',
-  functional: true,
-  props: {
-    daemonId: {
-      type: [Number, String],
-      default: 'daemon'
-    },
-    autoColse: {
-      type: Boolean,
-      default: true
-    }
-  },
-  render (h, { data, children, props }) {
-    props.view = 'view'
-    data.props = props
-    return h(master, data, children)
-  }
-}
+import { getOptions } from './master'
+export default getOptions('ddv-multi-window-view', 'view')
